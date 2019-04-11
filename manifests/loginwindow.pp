203f1d9bce8bb537ddb9f37bef130f373c024891
Andrew D. Montgomery <andrewdmontgomery@mac.com>
2019-04-11 18:43:26 -0500

    Refactored for new mobileconfig format, added a lot of praameters

--- manifests/loginwindow.pp
+++ manifests/loginwindow.pp
@@ -1,15 +1,52 @@
 # manage common loginwindow settings
 class mac_profile::loginwindow (
   $ensure                 = 'present',
-  $guest_enabled          = false,
-  $show_input_menu        = true,
-  $show_fullname          = true,
-  $hide_local_users       = true,
-  $include_network_user   = false,
-  $hide_admin_users       = true,
-  $hide_mobile_accounts   = true,
-  $disable_console_access = true,
-  $loginwindow_text       = ''
+  # PayloadDisplayName: `Login Window:  Global Preferences`
+  $fast_user_switching_enabled  = true,
+  $auto_logout_delay            = 0,
+
+  # PayloadDisplayName: `LoginWindow`
+  $skip_appearance_setup        = false,
+  $skip_cloud_setup             = false,
+  $skip_privacy_setup           = false,
+  $skip_siri_setup              = false,
+  $skip_truetone_setup          = false,
+  $skip_iCloud_storage_setup    = false,
+
+  # PayloadDisplayName: `Login Window:  Screen Saver Preferences`
+  $idle_time                    = 1200, # seconds
+  $login_window_idle_time       = $idle_time,
+
+  # PayloadDisplayName: `Login Window:  MCX Preferences`
+  $guest_enabled                = false,
+  $guest_disbled                = !$guest_enabled,
+
+  # PayloadDisplayName: `Login Window` (with a space)
+  ##$show_input_menu        = true,
+  $show_fullname                = true,
+  $hide_local_users             = true,
+  $include_network_user         = false,
+  $hide_admin_users             = true,
+  $hide_mobile_accounts         = true,
+  $disable_console_access       = true,
+  $local_user_login_enabled     = true,
+  $external_accounts_enabled    = false,
+  $restart_button_disabled      = false,
+  $sleep_button_disabled        = false,
+  $shutdown_button_disabled     = false,
+  $show_other_users_managed     = true,
+  $admin_may_disable_MCX        = true,
+  $disable_auto_login           = true,
+  $retries_until_hint           = 0,
+  $loginwindow_text             = '',
+
+  # PayloadDisplayName: `Login Window: Scripts`
+  $skip_login_hook              = false,
+  $skip_logout_hook             = false,
+
+  # PayloadDisplayName: `Settings for Devices`
+  $payload_removal_disallowed   = false
+
 ) inherits mac_profile {
 
   mac_profiles_handler::manage { "${profile_identifier_prefix}.loginwindow":
